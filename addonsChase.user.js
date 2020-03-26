d7b19c02e3a24c61bf4bbb25fd2e4da9703c396e
Tepirek <tepirek@outlook.com>
2020-03-26 17:22:34 +0100

    Update addonsChase.user.js

--- addonsChase.user.js
+++ addonsChase.user.js
@@ -16,7 +16,7 @@
     //elementy graficzne
     let box = d.createElement("span");
     box.id = "enepere123";
-    box.style = "position: absolute; z-index: 1000; color: white; top: 55px; left: 0px; min-width: 300px; font-size: 15px;";
+    box.style = "position:absolute;z-index:1000;min-width:380px;max-width:380px;color:white;top:0px;left:0px;font-size:16px;";
     d.body.appendChild(box);
     let uciek = d.createElement("span");
     uciek.innerHTML = "UCIECZKA";
@@ -27,42 +27,42 @@
     let isUcieczka = false;
     let idZwoja;
     window.hero.searchPath1 = function(a, t) {
-                if (this.isBlockedSearchPath()) return this.blockedInfoSearchPath();
-                for (var h = [], i = 128 & hero.opt ? 8 : 20, r = Math.max(0, Math.min(a, this.x) - i), s = Math.min(map.x - 1, Math.max(a, this.x) + i), o = Math.max(0, Math.min(t, this.y) - i), e = Math.min(map.y - 1, Math.max(t, this.y) + i), d = r - 1; s + 1 >= d; d++) {
-                    h[d] = [];
-                    for (var x = o - 1; e + 1 >= x; x++) h[d][x] = !(d >= r && s >= d && x >= o && e >= x) || isset(g.npccol[d + 256 * x]) || map.col && "0" != map.col.charAt(d + x * map.x) ? -2 : -1
+        if (this.isBlockedSearchPath()) return this.blockedInfoSearchPath();
+        for (var h = [], i = 128 & hero.opt ? 8 : 20, r = Math.max(0, Math.min(a, this.x) - i), s = Math.min(map.x - 1, Math.max(a, this.x) + i), o = Math.max(0, Math.min(t, this.y) - i), e = Math.min(map.y - 1, Math.max(t, this.y) + i), d = r - 1; s + 1 >= d; d++) {
+            h[d] = [];
+            for (var x = o - 1; e + 1 >= x; x++) h[d][x] = !(d >= r && s >= d && x >= o && e >= x) || isset(g.npccol[d + 256 * x]) || map.col && "0" != map.col.charAt(d + x * map.x) ? -2 : -1
+        }
+        h[this.x][this.y] = 0, b = -1, road = [];
+        for (var c = {
+            x: -1,
+            y: -1,
+            dist: 99
+        }, y = 1; s - r + e - o + 3 > y; y++)
+            for (var d = r; s >= d; d++)
+                for (var x = o; e >= x; x++) {
+                    if (-1 != h[d][x] || h[d][x - 1] != y - 1 && h[d][x + 1] != y - 1 && h[d - 1][x] != y - 1 && h[d + 1][x] != y - 1 || (h[d][x] = y), h[a][t] > 0) {
+                        d = s + 1;
+                        break
+                    }
+                    c.dist2 = Math.abs(a - d) + Math.abs(t - x), h[d][x] == y && c.dist2 < c.dist && (c.x = d, c.y = x, c.dist = c.dist2)
                 }
-                h[this.x][this.y] = 0, b = -1, road = [];
-                for (var c = {
-                    x: -1,
-                    y: -1,
-                    dist: 99
-                }, y = 1; s - r + e - o + 3 > y; y++)
-                    for (var d = r; s >= d; d++)
-                        for (var x = o; e >= x; x++) {
-                            if (-1 != h[d][x] || h[d][x - 1] != y - 1 && h[d][x + 1] != y - 1 && h[d - 1][x] != y - 1 && h[d + 1][x] != y - 1 || (h[d][x] = y), h[a][t] > 0) {
-                                d = s + 1;
-                                break
-                            }
-                            c.dist2 = Math.abs(a - d) + Math.abs(t - x), h[d][x] == y && c.dist2 < c.dist && (c.x = d, c.y = x, c.dist = c.dist2)
-                        }
-                if (c.hdist = Math.abs(a - hero.x) + Math.abs(t - hero.y), h[a][t] > 0 || c.dist < c.hdist) {
-                    h[a][t] < 0 && (a > c.x ? b = 2 : a < c.x ? b = 1 : t > c.y ? b = 0 : t < c.y && (b = 3), a = c.x, t = c.y), road[0] = {
-                        x: a,
-                        y: t
-                    };
-                    for (var f = h[a][t] - 1, l = a, m = t; f > 0; f--) h[l][m - 1] == f ? m-- : h[l][m + 1] == f ? m++ : h[l - 1][m] == f ? l-- : h[l + 1][m] == f ? l++ : f = 0, f && (road[h[a][t] - f] = {
-                        x: l,
-                        y: m
-                    })
-                }
-                road.length > 1 && null == g.playerCatcher.follow && $("#target").stop().css({
-                    left: 32 * a,
-                    top: 32 * t,
-                    display: "block",
-                    opacity: 1
-                }).fadeOut(1e3)
+        if (c.hdist = Math.abs(a - hero.x) + Math.abs(t - hero.y), h[a][t] > 0 || c.dist < c.hdist) {
+            h[a][t] < 0 && (a > c.x ? b = 2 : a < c.x ? b = 1 : t > c.y ? b = 0 : t < c.y && (b = 3), a = c.x, t = c.y), road[0] = {
+                x: a,
+                y: t
             };
+            for (var f = h[a][t] - 1, l = a, m = t; f > 0; f--) h[l][m - 1] == f ? m-- : h[l][m + 1] == f ? m++ : h[l - 1][m] == f ? l-- : h[l + 1][m] == f ? l++ : f = 0, f && (road[h[a][t] - f] = {
+                x: l,
+                y: m
+            })
+        }
+        road.length > 1 && null == g.playerCatcher.follow && $("#target").stop().css({
+            left: 32 * a,
+            top: 32 * t,
+            display: "block",
+            opacity: 1
+        }).fadeOut(1e3)
+    };
     uciek.addEventListener("click", function() {
         if (g.battle) {
             if (!isUcieczka) {
@@ -83,14 +83,13 @@
         }
     });
     box.appendChild(uciek);
-    let dane = d.createElement("span");
+    let dane = d.createElement("div");
+    dane.style = "min-width:360px";
     box.appendChild(dane);
-    let style = d.createElement("style");
-    style.innerHTML = ".adison9711:hover{background:#686868}";
-    d.head.appendChild(style);
     //zmienna numeracji
     let number;
     //zmienne dla poscigu
+    let attack = false;
     let oid = 0;
     let ox;
     let oy;
@@ -101,7 +100,8 @@
     d.querySelector("#nick").addEventListener("click", function() {
         if (oid != 0) {
             oid = 0;
-            message(`Przerwałeś pościg!`);
+            attack = false;
+            message(`Przerwano pościg lub śledzenie!`);
         }
     });
 
@@ -113,91 +113,87 @@
     }
 
     function createOtherSpan(nr, id, nick, klan, relacja, lvl, profa, prawa) {
-        if(klan == "Brak klanu" || klan == "Anioły Ciemności" || klan == "Archanioły Ciemności" || klan == "Nowe Porządki" || klan == "Libertas" || klan == "Zwykły Klan" || klan == "Ciemna strona pomidora" || klan == "Tańczący z Orkami" || klan == "Freedom" || klan == "Żołnierze Wyklęci") {
-        //caly div
-        let sp = d.createElement("span");
-        sp.classList.add("adison9711");
-        sp.innerHTML = `<br>${nr}. `;
-        //nick i relacja(style tez)
-        let nickname = d.createElement("span");
-        nickname.innerHTML = `${nick}`;
-        nickname.tip = "Naciśnij, aby napisać do tej osoby.";
-        nickname.addEventListener("click", function() {
+        let entry = d.createElement("div");
+        entry.style = "padding-top:2px;padding-bottom:2px;background:black";
+
+        let entryNumber = d.createElement("div");
+        entryNumber.style = "float:left;width:25px";
+        entryNumber.innerHTML = `${nr}. `;
+        entry.appendChild(entryNumber);
+
+        let entryNickname = d.createElement("div");
+        entryNickname.style = "float:left;width:130px";
+        entryNickname.innerHTML = `${nick} `;
+        entryNickname.addEventListener("click", function() {
             chatTo(`${nick}`);
         });
-        nickname.style.fontWeight = "bold";
-        if (prawa > 0) {
-            nickname.style.background = "gold";
+        switch (klan) {
+            case 'ARYSTOKRACJA':
+            case 'MAGNATERIA':
+            case 'Death Eaters':
+            case 'Arystokracja Ciemności':
+            case 'Trusted':
+            case 'Óseless':
+            case 'Phasianae':
+            case 'Szit Reality':
+            case 'N N J L':
+                entry.style.background = "#2c8535";
+                break;
+            case 'Anioły Ciemności':
+            case 'Archanioły Ciemności':
+            case 'Libertas':
+            case 'Nowe Porządki':
+            case 'Zwykły Klan':
+            case 'Arvania':
+            case 'Tańczący z Orkami':
+            case 'Weterani z Piekła Rodem':
+                entry.style.background = "#913131";
+                break;
         }
-        switch (relacja) {
-            case "fr":
-                sp.style.background = "#02cf39";
-                nickname.style.background = "#296639";
-                break;
-            case "en":
-                sp.style.background = "#ff0000";
-                nickname.style.background = "#912d2d";
-                break;
-            case "cl":
-                sp.style.background = "#00aeff";
-                nickname.style.background = "#36657a";
-                break;
-        }
-        sp.style.background = "#ff0000";
-        nickname.style.background = "#912d2d";
+        entry.appendChild(entryNickname);
 
-           if(klan == "Brak klanu") {
-               sp.style.background = "#2c6963";
-               nickname.style.background = "#2c6963";
-           }
-        sp.appendChild(nickname);
+        let entryLevel = d.createElement("div");
+        entryLevel.style = "float:left;width:40px";
+        entryLevel.innerHTML = `${lvl}${profa} `;
+        entry.appendChild(entryLevel);
 
-        //lvl i profa
-        let lvlpr = d.createElement("span");
-        lvlpr.innerHTML = ` ${lvl}${profa} `;
-        sp.appendChild(lvlpr);
+        let distance = Math.round(Math.sqrt(Math.pow(g.other[id].x - hero.x, 2) + Math.pow(g.other[id].y - hero.y, 2)));
+        if (distance < 10) distance = `0${distance}`;
+        let entryDistance = d.createElement("div");
+        entryDistance.style = "float:left;width:30px";
+        entryDistance.innerHTML = `[${distance}] `;
+        entry.appendChild(entryDistance);
 
-        //odleglosc
-        let odle = d.createElement("span");
-        let licz = Math.round(Math.sqrt(Math.pow(g.other[id].x - hero.x, 2) + Math.pow(g.other[id].y - hero.y, 2)));
-        odle.innerHTML = `[Odl.: ${licz}] `;
-        odle.tip = "Naciśnij, aby podejść."
-        odle.addEventListener("click", function() {
-            window.hero.searchPath1(g.other[id].x, g.other[id].y);
-        });
-        sp.appendChild(odle);
-
-        //poscig
-        let forposcig = d.createElement("span");
-        forposcig.innerHTML = `[ATAKUJ]`;
-        forposcig.tip = "Naciśnij, aby aktywować pościg."
-        forposcig.addEventListener("click", function() {
+        let entryChase = d.createElement("div");
+        entryChase.style = "float:left;width:75px";
+        entryChase.innerHTML = '[ATTACK]';
+        entryChase.addEventListener("click", function() {
             if (!g.battle) {
-                if (map.pvp == 2) {
+                if(map.pvp == 2) {
                     oid = id;
-                    message(`Rozpoczęto pościg za: ${g.other[id].nick}!`);
-                } else {
-                    message(`Nie jesteś na czerwonej mapce!`);
-                }
-            } else {
-                message(`Trwa walka!`);
-            }
+                    attack = true;
+                    message("Rozpoczęto pościg za: ${g.other[id].nick}!");
+                } else message("Nie jesteś na czerwonej mapie!");
+            } else message("Trwa walka!");
         });
-        sp.appendChild(forposcig);
+        entry.appendChild(entryChase);
 
-        //kolorek dla postaci po najechaniu
-        sp.addEventListener("mouseover", function() {
-            wyjebPodswietlenie();
-            d.querySelector(`#other${id}`).style.backgroundColor = "rgb(255, 102, 0, .65)";
-            d.querySelector(`#other${id}`).style.borderRadius = "7px";
+        let entryFollow = d.createElement("div");
+        entryFollow.style = "float:left;width:75px";
+        entryFollow.innerHTML = '[FOLLOW]';
+        entryFollow.addEventListener("click", function() {
+            if (!g.battle) {
+                oid = id;
+                attack = false;
+                message(`Rozpoczęto podążanie za: ${g.other[id].nick}!`);
+            } else message("Trwa walka!");
         });
-        sp.addEventListener("mouseleave", function() {
-            if (g.other[id]) d.querySelector(`#other${id}`).style.backgroundColor = "";
-        });
-        dane.appendChild(sp);
+        entry.appendChild(entryFollow);
 
-        // dodawanie divow
-        }
+        let entryClear = d.createElement("div");
+        entryClear.style = "clear:both";
+        entry.appendChild(entryClear);
+        dane.appendChild(entry);
     }
 
     //kreacja div'a
@@ -245,7 +241,7 @@
                 hx = hero.x;
                 hy = hero.y;
                 if (!g.battle) {
-                    if (Math.abs(hx - ox) <= 3 && Math.abs(hy - oy) <= 3) {
+                    if (attack && Math.abs(hx - ox) <= 1 && Math.abs(hy - oy) <= 1) {
                         if (!walczacy.includes(Number(oid))) _g(`fight&a=attack&id=${oid}`);
                     }
                     if (hx != ox || hy != oy) {
@@ -253,6 +249,7 @@
                     }
                 } else {
                     oid = 0;
+                    attack = false;
                     message(`Jest walka!`);
                 }
             }
@@ -261,24 +258,24 @@
     }
 
     let chudsada;
-battleMsg = function(a, b) {
-    let ret = _bM(a, b);
-    if (a.indexOf("winner=") > -1) {
-        _g("fight&a=quit");
-        if (isUcieczka) {
-            _g(`moveitem&st=1&id=${idZwoja}`);
-            isUcieczka = false;
-            uciek.style.color = "white";
-            uciek.innerHTML = "UCIECZKA";
-        }
-        chudsada = setInterval(function(){
-            if(g.battle){
-                _g("fight&a=quit");
-            } else {
-                clearInterval(chudsada);
+    battleMsg = function(a, b) {
+        let ret = _bM(a, b);
+        if (a.indexOf("winner=") > -1) {
+            _g("fight&a=quit");
+            if (isUcieczka) {
+                _g(`moveitem&st=1&id=${idZwoja}`);
+                isUcieczka = false;
+                uciek.style.color = "white";
+                uciek.innerHTML = "UCIECZKA";
             }
-        }, 100);
+            chudsada = setInterval(function(){
+                if(g.battle){
+                    _g("fight&a=quit");
+                } else {
+                    clearInterval(chudsada);
+                }
+            }, 300);
+        }
+        return ret;
     }
-    return ret;
-}
 })(document, parseInput, newNpc, hero.run, battleMsg)
